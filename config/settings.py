763f1215ebbdf6f326268cc02cd0f344845f4f1e
pisces2336 <pisces2336@gmail.com>
2024-11-25 01:12:05 +0900

    simplejwtインストール

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,6 +42,17 @@ INSTALLED_APPS = [
     "djoser",
     "apps.account",
 ]
+
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": ("JWT"),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+}
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
