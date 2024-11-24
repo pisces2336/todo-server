1eaa99ba1921b8517a1444ea9d98e9398ed63984
pisces2336 <pisces2336@gmail.com>
2024-11-25 01:33:47 +0900

    Userモデル作り直し

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,22 +1,8 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 from core.models import BaseModel
 
 
-class UserManager(BaseUserManager):
-    pass
-
-
-class User(BaseModel, AbstractBaseUser):
-    name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
-    last_login = None
-
-    USERNAME_FIELD = "email"
-
-    objects = UserManager
-
+class User(AbstractUser, BaseModel):
     class Meta:
         db_table = "user"
